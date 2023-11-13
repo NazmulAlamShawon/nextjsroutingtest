f8aa8d215a462276834598df13b50224b72d9886
NazmulAlamShawon <shawonkst15@gmail.com>
2023-11-13 17:38:52 +0600

    add new navbar

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -6,7 +6,7 @@ export default function DashboardLayout({
   return (
     <section>
       {/* Include shared UI here e.g. a header or sidebar */}
-      <nav></nav>
+      <nav> <h1>navbar</h1></nav>
  
       {children}
     </section>
